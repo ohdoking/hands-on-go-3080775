e0a400433cb716afe38b926f10ecfdbcf3a749fa
Kenny Oh Dokeun <ohdoking@gmail.com>
2022-11-12 05:37:10 +0000

    solve the problem for package challenges

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -2,9 +2,18 @@
 package main
 
 // import the proverbs package
+import (
+	"fmt"
+	gp "github.com/jboursiquot/go-proverbs"
+)
 
 // getRandomProverb returns a random proverb from the proverbs package
+func getRandomProverb() (saying string) {
+	saying = gp.Random().Saying
+	return 
+}
 
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomProverb())
 }
